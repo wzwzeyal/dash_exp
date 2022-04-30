d7aa7cba0f28b9674f2a8ef2b5dfb0bf39d7230b
Ub_eyal <wzwzeyal@gmail.com>
2022-04-30 21:40:27 +0300

    add sql support table formatting

--- dash_tag_view/layout/details/layout.py
+++ dash_tag_view/layout/details/layout.py
@@ -1,21 +1,43 @@
+import dash_bootstrap_components as dbc
 from dash import dcc, html
 
 details_layout = html.Div(
     [
-        dcc.Textarea(
+        dbc.Textarea(
             id='left-textarea-example',
             value='left-textarea-example',
             readOnly=True,
-            style={'width': '48%', 'height': 400, 'direction': 'rtl', 'border-style': 'none', 'margin-right': '2%'},
+            style={
+                'width': '49%',
+                'height': 400,
+                'direction': 'rtl',
+                'border-style': 'none',
+                # 'margin-right': '2%',
+                'display': 'inline'
+            },
 
         ),
 
-        dcc.Textarea(
-            id='right-textarea-example',
+        # html.Spacer(
+        #     style={
+        #         'width': '10%',
+        #         'display': 'inline',
+        #     }
+        # ),
 
+        dbc.Textarea(
+            id='right-textarea-example',
             value='right-textarea-example',
             readOnly=True,
-            style={'width': '50%', 'height': 400, 'direction': 'rtl', 'border-style': 'none'},
+            style={
+                'width': '49%',
+                'height': 400,
+                'direction': 'rtl',
+                'border-style': 'none',
+                'display': 'inline',
+                'position': 'absolute',
+                'right': 10,
+            },
             # 'margin': '2%'},  # 'margin-left': "10%"},
 
         ),
@@ -39,5 +61,5 @@ details_layout = html.Div(
         ),
     ],
     id="details",
-    style={'margin': 10, "borderStyle": "groove"}
+    style={'margin': 10, "borderStyle": "none"}
 )
